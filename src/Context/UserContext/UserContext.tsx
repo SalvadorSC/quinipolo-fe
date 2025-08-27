ae2d757654216b6f9a02648cf921cf35673ee80d
salvador.sanchez@king.com <salvador.sanchez@king.com>
2025-08-27 12:21:02 +0200

    small fix

--- src/Context/UserContext/UserContext.tsx
+++ src/Context/UserContext/UserContext.tsx
@@ -118,7 +118,8 @@ export const UserProvider = ({ children }: UserProviderProps) => {
   // Function to manually refresh user data
   const refreshUserData = useCallback(async () => {
     console.log("refreshUserData");
-    console.log("userData", userData);
+    console.log("userData.isAuthenticated", userData.isAuthenticated);
+    console.log("userData.userId", userData.userId);
     if (userData.isAuthenticated && userData.userId) {
       try {
         const profile = await apiGet<UserDataType>("/api/users/me/profile");
